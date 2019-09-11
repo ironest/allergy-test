e4cd1d08efc2df14884008f8ef78d116d9f45d28
ironest <ironest@gmail.com>
2019-09-11 22:53:50 +1000

    Constant declaration

---
+++ index.rb
@@ -0,0 +1,11 @@
+# Constant declaration
+ALLERGY_DATA = {
+    "eggs":         1,
+    "peanuts":      2,
+    "shellfish":    4,
+    "strawberries": 8,
+    "tomatoes":     16,
+    "chocolate":    32,
+    "pollen":       64,
+    "cats":         128,
+}
